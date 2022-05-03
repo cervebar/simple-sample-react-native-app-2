e8d664d85e7eca345ec739b5d1f6f223da848579
Barbora Cervenkova <cervenkovab@gmail.com>
2022-05-03 11:40:18 +0200

    starter

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.wrapper}>
+          <Text>Success!</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.lighter,
+  },
+});
+
+export default App;
